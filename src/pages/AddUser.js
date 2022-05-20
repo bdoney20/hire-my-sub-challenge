6d25873bbb6392212597669de9bebf0c1354fab5
bdoney20 <71524255+bdoney20@users.noreply.github.com>
2022-05-19 21:04:48 -0600

    Updates

--- src/pages/AddUser.js
+++ src/pages/AddUser.js
@@ -9,7 +9,7 @@ class AddUser extends React.Component {
             last_name: '',
             email: '',
             occupation: '',
-            submitted: false
+            message: '',
         }
     
         this.handleChange = this.handleChange.bind(this);
@@ -30,51 +30,60 @@ class AddUser extends React.Component {
             occupation: this.state.occupation
         })
         .then(res => {
-            this.setState({'submitted': true})
-            console.log('The post worked')
-            console.log(res)
-            console.log(this.state)
+            this.setState({'message': res.data.message})
         })
         .catch(err => {
             console.log(err)
+            this.setState({'message': 'Sorry, something went wrong! Please try again later'})
         })
       }
 
 
   render() {
      return (
-        <div className='container mx-auto px-10'>
-        <form onSubmit={this.handleSubmit}>
+        <div className='container mx-auto p-10'>
+            {this.state.message && (
+                <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 pb-5" role="alert">
+                    <p className="font-bold">{this.state.message}</p>
+                </div>
+            )}
+            
+        <form onSubmit={this.handleSubmit} className='pt-5'>
             <div className="grid xl:grid-cols-2 xl:gap-6">
                 <div className="relative z-0 w-full mb-6 group">
-                    <input type="text" name="first_name" className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required onChange={this.handleChange}/>
-                    <label htmlFor="first_name" className="peer-focus:font-medium  text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                    <input type="text" name="first_name" className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none" placeholder=" " required onChange={this.handleChange}/>
+                    <label htmlFor="first_name" className="peer-focus:font-medium  text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10">
                         First name
                     </label>
                 </div>
                 <div className="relative z-0 w-full mb-6 group">
-                    <input type="text" name="last_name" className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required onChange={this.handleChange}/>
-                    <label htmlFor="last_name" className="peer-focus:font-medium text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                    <input type="text" name="last_name" className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none" placeholder=" " required onChange={this.handleChange}/>
+                    <label htmlFor="last_name" className="peer-focus:font-medium text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3">
                         Last name
                     </label>
                 </div>
             </div>
             <div className="grid xl:grid-cols-2 xl:gap-6">
                 <div className="relative z-0 w-full mb-6 group">
-                    <input type="email" name="email" className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required onChange={this.handleChange}/>
-                    <label htmlFor="email" className="peer-focus:font-medium text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                    <input type="email" name="email" className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none" placeholder=" " required onChange={this.handleChange}/>
+                    <label htmlFor="email" className="peer-focus:font-medium text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3">
                         Email
                     </label>
                 </div>
                 <div className="relative z-0 w-full mb-6 group">
-                    <input type="text" name="occupation" className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required onChange={this.handleChange}/>
-                    <label htmlFor="occupation" className="peer-focus:font-medium  text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                    <input type="text" name="occupation" className="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none" placeholder=" " required onChange={this.handleChange}/>
+                    <label htmlFor="occupation" className="peer-focus:font-medium  text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3">
                        Occupation
                     </label>
                 </div>
             </div>
-            <button type="submit" value='Submit' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 float-right">Submit</button>
+            {this.state.first_name && this.state.last_name && this.state.email && this.state.occupation
+                ? <button type="submit" value='Submit' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center float-right">Add User</button>
+                : <button type="submit" value='Submit' className="text-white bg-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 float-right" disabled>Add User</button>
+            }
+            
         </form>
+        
         </div>
     );
   }
